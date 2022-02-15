4704b8d656e2a2b78bb5dd232cd498a2c5112905
ockev <koconnor@hashicorp.com>
2022-02-15 10:47:01 -0500

    test commit

--- project-dev/compute.tf
+++ project-dev/compute.tf
@@ -60,7 +60,7 @@ resource "aws_instance" "app-server" {
 }
 
 module "s3-bucket" {
-  source  = "app.terraform.io/koconnor/s3-bucket/aws"
-  version = "2.6.0"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.14.1"
   # insert required variables here
 }
