1270758f54edcdb75b695a27bfaee4f2e1d674c6
ockev <koconnor@hashicorp.com>
2023-03-01 09:42:03 -0500

    test commit

--- project-qa/compute.tf
+++ project-qa/compute.tf
@@ -58,3 +58,9 @@ resource "aws_instance" "app-server" {
         Name = "kevin-${var.env}-app-server"
     }
 }
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.14.1"
+  # insert required variables here
+}
