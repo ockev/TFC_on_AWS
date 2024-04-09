95bd9646fd4a77dd330d97676ef5aa7eb025e448
ockev <koconnor@hashicorp.com>
2024-04-09 13:19:30 -0400

    packer updates

--- project-dev/compute.tf
+++ project-dev/compute.tf
@@ -5,24 +5,18 @@ provider "aws" {
   region = var.region
 }
 
-# data "hcp_packer_iteration" "ubuntu" {
-#   bucket_name = "learn-packer-run-tasks"
-#   channel     = "production"
-# }
-
-# data "hcp_packer_image" "ubuntu_us_east_2" {
-#   bucket_name    = "learn-packer-run-tasks"
-#   cloud_provider = "aws"
-#   iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
-#   region         = "us-east-2"
-# }
-
-data "hcp_packer_artifact" "learn-packer-run-tasks" {
-  bucket_name   = "learn-packer-run-tasks"
-  channel_name  = "production"
-  platform      = "aws"
-  region        = "us-east-2"
+data "hcp_packer_iteration" "ubuntu" {
+  bucket_name = "learn-packer-run-tasks"
+  channel     = "production"
 }
+
+data "hcp_packer_image" "ubuntu_us_east_2" {
+  bucket_name    = "learn-packer-run-tasks"
+  cloud_provider = "aws"
+  iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
+  region         = "us-east-2"
+}
+
 
 resource "aws_instance" "app_server" {
   ami           = data.hcp_packer_image.ubuntu_us_east_2.cloud_image_id
